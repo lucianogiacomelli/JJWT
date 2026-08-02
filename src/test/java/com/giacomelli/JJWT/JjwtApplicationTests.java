47580a78a0939afe4307a673c813a477fc566bc0
lucianogiacomelli <lucianogiacomelli8@gmail.com>
2026-08-02 19:43:01 -0300

    Initial commit

---
+++ src/test/java/com/giacomelli/JJWT/JjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giacomelli.JJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
